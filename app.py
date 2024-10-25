b4278e7d21d6e06aa01b86deef11e2b91c8ed048
dvconsultores <adominguez@dvconsultores.com>
2024-10-24 20:08:10 -0400

    Refactor app.py and update message translations

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import multiprocessing
+import time
 from flask import Flask
 from flask_migrate import Migrate
 from dotenv import load_dotenv
@@ -21,8 +23,33 @@ from controllers import all_blueprints
 for bp in all_blueprints:
     app.register_blueprint(bp)
 
+# Function to run the Flask app
+def run_flask_app():
+    os.system("gunicorn -c gunicorn_config.py app:app")
+
+# Function to run flask in debug mode
+def run_flask_app_debug():
+    app.run(debug=True)    
+
+# Function to run forever.py
+def run_forever_script():
+    import forever  # Import the forever.py script
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # # Start the Flask app in a separate process
+    # flask_process = multiprocessing.Process(target=run_flask_app)
+    # flask_process.start()
+
+    # # Start the forever.py script in a separate process
+    # forever_process = multiprocessing.Process(target=run_forever_script)
+    # forever_process.start()
+
+    # # Join processes to keep the main program running
+    # flask_process.join()
+    # forever_process.join()
+
+    run_flask_app_debug()
+
 
 # export FLASK_APP=app.py
 # flask db init
