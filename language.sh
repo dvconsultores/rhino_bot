63827848c4a4f261284ffdaba485779468d7e677
dvconsultores <adominguez@dvconsultores.com>
2025-01-11 19:19:34 -0400

    Remove unused language compilation script and application run script; add Gunicorn to requirements for improved performance

--- language.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Compile translations
-msgfmt telegram_bot/locales/es/LC_MESSAGES/messages.po -o telegram_bot/locales/es/LC_MESSAGES/messages.mo
-msgfmt telegram_bot/locales/en/LC_MESSAGES/messages.po -o telegram_bot/locales/en/LC_MESSAGES/messages.mo
-
-
-echo "Setup completed successfully."
